335eb64a9d31217c06f26c934fedb700e96ae9aa
cacutler <116305038+cacutler@users.noreply.github.com>
2026-01-21 10:00:21 -0700

    Added test files

---
+++ PointTrackerTests/PointTrackerTests.swift
@@ -0,0 +1,9 @@
+//  PointTrackerTests.swift
+//  PointTrackerTests
+//  Created by Cameron Alexander Cutler on 1/21/26.
+import Testing
+struct PointTrackerTests {
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
---
+++ PointTrackerUITests/PointTrackerUITestsLaunchTests.swift
@@ -0,0 +1,23 @@
+//  PointTrackerUITestsLaunchTests.swift
+//  PointTrackerUITests
+//  Created by Cameron Alexander Cutler on 1/21/26.
+import XCTest
+final class PointTrackerUITestsLaunchTests: XCTestCase {
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
